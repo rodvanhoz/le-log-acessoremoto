a6fd6af40b6eab1b54e86c25b3346813c761294c
Rodrigo Vanhoz Ribeiro <rodrigo.vanhoz@gmail.com>
2019-05-06 09:21:02 -0300

    Opção de digitar o argumento dentro da execução

--- LeAcessoRemoto.bat
+++ LeAcessoRemoto.bat
@@ -1,9 +1,22 @@
 @ECHO OFF
 
+SET SERVIDOR_TS=%1
+
+setlocal ENABLEDELAYEDEXPANSION
+
+:: verifica se foi digitado parametro
+if "!SERVIDOR_TS!" equ "" (
+	echo Informe o TS.
+	SET /P SERVIDOR_TS=
+	
+	if "!SERVIDOR_TS!" equ "" GOTO :EOF
+)
+
 copy /b \\srvv09\sys\BIN\Acesso_Remoto_Clientes\Conectados\AcessoRemoto.log
 echo.
 
-cscript //nologo LeAcessoRemoto.vbs %1 AcessoRemoto.log %2
+cscript //nologo LeAcessoRemoto.vbs !SERVIDOR_TS! AcessoRemoto.log
+
+endlocal
 
 del %cd%\AcessoRemoto.log
-
